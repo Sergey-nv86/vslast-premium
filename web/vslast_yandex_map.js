9fcbf691bb12dd423e91939cd23640b49c03261b
Sergey-nv86 <9129399754@mail.ru>
2026-09-09 11:56:16 +0500

    feat: add web map controls

--- web/vslast_yandex_map.js
+++ web/vslast_yandex_map.js
@@ -17,18 +17,10 @@
 
             if (!element) {
               attempts++;
-
               if (attempts >= maxAttempts) {
-                reject(
-                  new Error(
-                    'Yandex map element not found after waiting: ' +
-                      elementId
-                  )
-                );
-
+                reject(new Error('Yandex map element not found after waiting: ' + elementId));
                 return;
               }
-
               setTimeout(tryCreate, 50);
               return;
             }
@@ -41,58 +33,38 @@
                 {
                   center: [latitude, longitude],
                   zoom: zoom || 15,
-                  controls: []
+                  controls: ['zoomControl', 'geolocationControl']
                 },
-                {
-                  suppressMapOpenBlock: true
-                }
+                { suppressMapOpenBlock: true }
               );
 
               var marker = new ymaps.Placemark(
                 [latitude, longitude],
                 {},
-                {
-                  preset: 'islands#brownDotIcon',
-                  draggable: false
-                }
+                { preset: 'islands#brownDotIcon', draggable: false }
               );
 
               map.geoObjects.add(marker);
 
-              self.maps[elementId] = {
-                map: map,
-                marker: marker
-              };
+              self.maps[elementId] = { map: map, marker: marker };
 
-              map.events.add(
-                'boundschange',
-                function (event) {
-                  if (!event.get('newZoom')) {
-                    return;
-                  }
+              map.events.add('boundschange', function (event) {
+                if (!event.get('newZoom')) return;
 
-                  var center = map.getCenter();
-
-                  window.dispatchEvent(
-                    new CustomEvent('vslast-map-moved', {
-                      detail: {
-                        id: elementId,
-                        latitude: center[0],
-                        longitude: center[1],
-                        zoom: map.getZoom()
-                      }
-                    })
-                  );
-                }
-              );
-
-              window.dispatchEvent(
-                new CustomEvent('vslast-map-ready', {
+                var center = map.getCenter();
+                window.dispatchEvent(new CustomEvent('vslast-map-moved', {
                   detail: {
-                    id: elementId
+                    id: elementId,
+                    latitude: center[0],
+                    longitude: center[1],
+                    zoom: map.getZoom()
                   }
-                })
-              );
+                }));
+              });
+
+              window.dispatchEvent(new CustomEvent('vslast-map-ready', {
+                detail: { id: elementId }
+              }));
 
               resolve(map);
             } catch (error) {
@@ -105,36 +77,22 @@
       });
     },
 
-    moveTo: function (
-      elementId,
-      latitude,
-      longitude,
-      zoom
-    ) {
+    moveTo: function (elementId, latitude, longitude, zoom) {
       var item = this.maps[elementId];
-
-      if (!item) {
-        return;
-      }
+      if (!item) return;
 
       item.map.setCenter(
         [latitude, longitude],
         zoom || item.map.getZoom(),
-        {
-          duration: 400
-        }
+        { duration: 400 }
       );
     },
 
     getCenter: function (elementId) {
       var item = this.maps[elementId];
-
-      if (!item) {
-        return null;
-      }
+      if (!item) return null;
 
       var center = item.map.getCenter();
-
       return {
         latitude: center[0],
         longitude: center[1],
@@ -144,10 +102,7 @@
 
     destroy: function (elementId) {
       var item = this.maps[elementId];
-
-      if (!item) {
-        return;
-      }
+      if (!item) return;
 
       item.map.destroy();
       delete this.maps[elementId];
