1c50a0fb07b200c096c80d68e8783f7205f83b98
Sergey-nv86 <9129399754@mail.ru>
2026-09-23 16:36:14 +0500

    Mark release 26 build

--- lib/core/build_info.dart
+++ lib/core/build_info.dart
@@ -2,7 +2,7 @@
 // Keep this value synchronized with the release commit.
 
 const String buildDate = '2026-09-23';
-const String buildTime = '14:15:00';
-const String buildId = '20260923-141500';
-const String buildCommit = 'release-25';
-const String buildLabel = 'build 20260923-141500 · release 25';
+const String buildTime = '16:30:00';
+const String buildId = '20260923-163000';
+const String buildCommit = 'release-26';
+const String buildLabel = 'build 20260923-163000 · release 26';
